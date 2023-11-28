2cb30db3a6381b09283df12a12194651b2fbb600
sydje7 <144898932+sydje7@users.noreply.github.com>
2023-11-28 14:18:19 +0100

    sydney

--- O1A/app.js
+++ O1A/app.js
@@ -1,10 +1,27 @@
 class App
 {
     runApplication()
-        {
-            console.log("hello world!");
-            //code gaat hier
-        }
-}
-let app = new App();
+        { 
+            console.log("hello world")
+            let appNaam = "demoApp"; // string
+            let versienummer = 0.1; // double
+            let versiedatum = new Date("2023-11-23"); // Datum (voor extra uitdaging)
+            let auteur = "Sydney Cairo"; // string
+            let copyright = "Sydney BV"; // string
+            let distributeur = "Leerling soft"; // string
+            let darkmode = false; // boolean
+          
+            console.log(appNaam);
+            console.log(versienummer);
+            console.log(versiedatum);
+            console.log(auteur);
+            console.log(copyright);
+            console.log(distributeur);
+            console.log(darkmode);
+          
+          }
+          
+              }
+
+let app = new App()
 app.runApplication();
