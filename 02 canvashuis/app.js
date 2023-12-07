5b74db944ec53d236382e7e73a8eb064ac9720d3
sydje7 <144898932+sydje7@users.noreply.github.com>
2023-12-07 14:12:56 +0100

    sydney

--- 02 canvashuis/app.js
+++ 02 canvashuis/app.js
@@ -5,8 +5,12 @@ class App
             let canvas = document.getElementById("canvasId")
             let g = canvas.getContext("2d");
             g.fillRect(0,0,10,10);
+            g.fillStyle = "red";
+            g.fillRect(0,0,canvas.width,canvas.height);
 
-
+            g.fillStyle = "#000000";
+            g.fillRect(0,0,10,10);
+            
             console.log("hello world");
             console.log("canvas");
             // code gaat hier
