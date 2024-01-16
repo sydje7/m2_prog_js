262dcafb27c267a5e955c10af7b5d78f341052ca
sydje7 <144898932+sydje7@users.noreply.github.com>
2024-01-16 14:17:30 +0100

    sydney

--- 03 ifelse/app.js
+++ 03 ifelse/app.js
@@ -3,19 +3,14 @@ class App
     runApplication()
         { 
             console.log("hello world");
+            //hier komt je code
+            //dit is code commentaar voor javascript
+            //dit is ook commentaar
 
+          }
           
               }
-            }
-            
-let app = new App()
-app.runApplication();
-class Greet
-{
-    constructor()
-    {
-        console.log{'Greetings!'};
 
-    }
-}
-let Greet = new Greet{}
+let app = new App();
+app.runApplication();
+
